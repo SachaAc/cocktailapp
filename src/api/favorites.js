a115f8917ff432d748f1b0371555805f122c4c1a
SachaAc <its_me_5a5@hotmail.com>
2026-04-13 15:33:43 +0200

    login poging 2

--- src/api/favorites.js
+++ src/api/favorites.js
@@ -1,10 +1,19 @@
-import axios from "axios";
+import api from "../api/axios";
 
-export const addFavorite = async (cocktail) => {
-    return await axios.post("/favorites", cocktail);
+export const addFavorite = async (cocktail, userId) => {
+    return await api.post("/api/favorites", {
+        idDrink: cocktail.idDrink,
+        strDrink: cocktail.strDrink,
+        strDrinkThumb: cocktail.strDrinkThumb,
+        userId: userId
+    });
 };
 
 export const getFavorites = async () => {
-    const res = await axios.get("/favorites");
+    const res = await api.get("/api/favorites");
     return res.data;
+};
+
+export const deleteFavorite = async (id) => {
+    return await api.delete(`/api/favorites/${id}`);
 };
