2d3cf2c4c36927f995de815d825bce0964a31ad4
SachaAc <its_me_5a5@hotmail.com>
2026-04-26 12:19:35 +0200

    crash verholpen

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.jsx';
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <Router>
